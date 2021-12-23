e9c7999ccf46e635ff8d578341362b982544140d
scarjkee <frigtfullscar@mail.ru>
2021-12-23 23:15:55 +0500

    2 commits

---
+++ python/lesson1/why_vars_end.py
@@ -0,0 +1,20 @@
+##программа умный попугайчик
+name = "Кеша"
+age = 2
+period = 2
+print("попугайчик")
+print(name)
+print("в")
+print(age)
+print("года")
+print("научился говрить")
+print("свое имя")
+print(name)
+print()
+print("Через")
+print(period)
+print("года")
+print("он научился говрить")
+print("сколько ему лет")
+print(age + period)
+
