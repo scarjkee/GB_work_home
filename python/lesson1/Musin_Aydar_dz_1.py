6c3b210440223d970df24cff5554cc72dd248ecc
scarjkee <frigtfullscar@mail.ru>
2021-12-26 00:38:56 +0500

    Musin_Aydar_dz_1.1

--- python/lesson1/Musin_Aydar_dz_1.py
+++ python/lesson1/Musin_Aydar_dz_1.py
@@ -5,9 +5,13 @@ def convert_time(duration: int) -> str:
     day = duration // 86400
     hour = (duration - (day * 86400)) // 3600
     minutes = (duration - (3600 * (duration // 3600))) // 60
-    seconds = (duration - (3600 * (duration // 3600))) // 60
-    return
+    seconds = (duration - (3600 * (duration // 3600))) - (minutes * 60)
+    return day, hour, minutes, seconds
 
-duration = 400153
+
+duration = 4153
+result = convert_time(duration)
+print(result)
+
 #result = convert_time(duration)
-print(day, hour, minutes, seconds)
+
