85d705cfbeaa87cee9b294442ecb296d2eefa06e
scarjkee <frigtfullscar@mail.ru>
2021-12-29 22:15:04 +0500

    Musin_Aydar_task_2.1

---
+++ python/leson02/Musin_Aydar_dz_1.1.py
@@ -0,0 +1,11 @@
+code = [15 * 3, 15 / 3, 15 // 2, 15 ** 2]
+
+def result_div(list):
+    result = []
+    for i in list:
+        result = (type(i))
+        print(result)
+
+    return
+
+result_1 = result_div(code)
